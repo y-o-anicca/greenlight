ec267485c19ec19295ad01fd448191e803a4a6f9
y-o-anicca <yodog712@gmail.com>
2022-07-07 14:33:59 +0900

    認証機能追加

--- internal/data/users.go
+++ internal/data/users.go
@@ -15,6 +15,8 @@ var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -32,6 +34,10 @@ type password struct {
 
 type UserModel struct {
 	DB *sql.DB
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func (m UserModel) Insert(user *User) error {
